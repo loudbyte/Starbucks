af6aea5e6d605442e8f32874ffc4994275d6bba8
loudbyte <example@gmail.com>
2020-01-30 16:59:52 +0600

    Demo Version 2

--- src/com/epam/starbucks/main/Order.java
+++ src/com/epam/starbucks/main/Order.java
@@ -9,7 +9,6 @@ import com.epam.starbucks.products.coffee.beans.types.*;
 import java.io.IOException;
 import java.util.*;
 
-
 public class Order {
     public static void main(String[] args) throws IOException {
 
